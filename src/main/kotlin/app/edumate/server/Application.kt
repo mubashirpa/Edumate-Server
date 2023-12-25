cfd92b0c69db57efcf4524a9254a6d3a4de8c7af
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-25 15:41:55 +0530

    Add Firebase components

--- src/main/kotlin/app/edumate/server/Application.kt
+++ src/main/kotlin/app/edumate/server/Application.kt
@@ -1,13 +1,17 @@
 package app.edumate.server
 
 import app.edumate.server.data.remote.OneSignalServiceImpl
+import app.edumate.server.plugins.configureFirebase
 import app.edumate.server.plugins.configureRouting
 import app.edumate.server.plugins.configureSerialization
+import com.google.cloud.firestore.Firestore
+import com.google.firebase.cloud.FirestoreClient
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -15,16 +19,17 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     configureSerialization()
+    configureFirebase()
 
     val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             json()
         }
     }
+    val oneSignalApiKey = environment.config.propertyOrNull("onesignal.api_key")?.getString().orEmpty()
+    val oneSignalAppId = environment.config.propertyOrNull("onesignal.app_id")?.getString().orEmpty()
+    val oneSignalService = OneSignalServiceImpl(client, oneSignalApiKey)
+    val firebaseFirestore: Firestore = FirestoreClient.getFirestore()
 
-    val apiKey = environment.config.propertyOrNull("onesignal.api_key")?.getString().orEmpty()
-    val appId = environment.config.propertyOrNull("onesignal.app_id")?.getString().orEmpty()
-    val service = OneSignalServiceImpl(client, apiKey)
-
-    configureRouting(service, appId)
+    configureRouting(oneSignalService, oneSignalAppId, firebaseFirestore)
 }
