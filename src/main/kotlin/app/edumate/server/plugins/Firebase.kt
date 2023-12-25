acad2c2b48f88137c3cc4f273579804b2c1af3c3
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-25 15:40:21 +0530

    Add Firebase.kt

---
+++ src/main/kotlin/app/edumate/server/plugins/Firebase.kt
@@ -0,0 +1,19 @@
+package app.edumate.server.plugins
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import io.ktor.server.application.*
+import java.io.FileInputStream
+
+fun Application.configureFirebase() {
+    val databaseUrl = environment.config.propertyOrNull("firebase.database_url")?.getString().orEmpty()
+    val refreshToken = FileInputStream("src/main/resources/edu-mate-app-firebase-adminsdk.json")
+
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(refreshToken))
+        .setDatabaseUrl(databaseUrl)
+        .build()
+
+    FirebaseApp.initializeApp(options)
+}
