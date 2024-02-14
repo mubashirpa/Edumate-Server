e9d998d149b3d707d3adb76768e6511a64e20abc
Mubashir P A <mubashirpa2002@gmail.com>
2024-02-14 14:40:45 +0530

    Added CoursesRoutes.

--- src/main/kotlin/app/edumate/server/plugins/Routing.kt
+++ src/main/kotlin/app/edumate/server/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package app.edumate.server.plugins
 
 import app.edumate.server.data.remote.OneSignalService
+import app.edumate.server.routes.coursesRouting
 import app.edumate.server.routes.firebaseRouting
 import app.edumate.server.routes.notificationRouting
 import com.google.cloud.firestore.Firestore
@@ -10,10 +11,11 @@ import io.ktor.server.routing.*
 fun Application.configureRouting(
     oneSignalService: OneSignalService,
     oneSignalAppId: String,
-    firebaseFirestore: Firestore
+    firebaseFirestore: Firestore,
 ) {
     routing {
-        notificationRouting(oneSignalService, oneSignalAppId)
+        coursesRouting()
         firebaseRouting(firebaseFirestore)
+        notificationRouting(oneSignalService, oneSignalAppId)
     }
 }
