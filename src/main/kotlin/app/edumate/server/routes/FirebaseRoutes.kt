4da954b2e97ec7571212bbe9d44846c64973e858
Mubashir P A <mubashirpa2002@gmail.com>
2024-02-14 14:55:22 +0530

    Implemented calling data from firebase firestore.

--- src/main/kotlin/app/edumate/server/routes/FirebaseRoutes.kt
+++ src/main/kotlin/app/edumate/server/routes/FirebaseRoutes.kt
@@ -1,23 +1,38 @@
 package app.edumate.server.routes
 
-import com.google.cloud.firestore.DocumentReference
+import com.google.api.core.ApiFuture
 import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.Query
+import com.google.cloud.firestore.QuerySnapshot
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.firebaseRouting(database: Firestore) {
-    route("/firestore") {
-        get {
-            val reference: DocumentReference = database.collection("cities").document("SF")
-            val future = reference.get()
-            val document = future.get()
+fun Route.firebaseRouting(firebaseFirestore: Firestore) {
+    route("/v1/courses") {
+        get("{userId}/{courseStates?}/{pageSize?}/{pageToken?}/{studentId?}/{teacherId?}") {
+            val userId =
+                call.parameters["userId"] ?: return@get call.respondText(
+                    "You must specify an userId.",
+                    status = HttpStatusCode.BadRequest,
+                )
 
-            if (document.exists()) {
-                call.respondText("${document.data}", status = HttpStatusCode.OK)
+            val query: ApiFuture<QuerySnapshot> =
+                firebaseFirestore.collection("courses")
+                    .whereArrayContains("teacherGroupId", userId)
+                    .orderBy("name", Query.Direction.ASCENDING)
+                    .limit(10).get()
+            val querySnapshot = query.get()
+            val documents = querySnapshot.documents
+
+            if (documents.isNotEmpty()) {
+                for (document in documents) {
+                    println(document.id + " => " + document.data["name"])
+                }
+                call.respondText("$documents", status = HttpStatusCode.OK)
             } else {
-                call.respondText("No such document!", status = HttpStatusCode.InternalServerError)
+                call.respondText("No such document!", status = HttpStatusCode.NotFound)
             }
         }
     }
