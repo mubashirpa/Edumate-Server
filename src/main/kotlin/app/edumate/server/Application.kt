3d5035f751e2343a556159ac3065672dd9ece21f
Mubashir P A <mubashirpa2002@gmail.com>
2024-02-14 14:41:23 +0530

    Installed CORS and Resources.

--- src/main/kotlin/app/edumate/server/Application.kt
+++ src/main/kotlin/app/edumate/server/Application.kt
@@ -4,32 +4,43 @@ import app.edumate.server.data.remote.OneSignalServiceImpl
 import app.edumate.server.plugins.configureFirebase
 import app.edumate.server.plugins.configureRouting
 import app.edumate.server.plugins.configureSerialization
-import com.google.cloud.firestore.Firestore
 import com.google.firebase.cloud.FirestoreClient
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.resources.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(CORS) {
+        anyHost()
+    }
+    install(Resources)
+
+    val client =
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
     configureSerialization()
     configureFirebase()
 
-    val client = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json()
-        }
-    }
     val oneSignalApiKey = environment.config.propertyOrNull("onesignal.api_key")?.getString().orEmpty()
     val oneSignalAppId = environment.config.propertyOrNull("onesignal.app_id")?.getString().orEmpty()
     val oneSignalService = OneSignalServiceImpl(client, oneSignalApiKey)
-    val firebaseFirestore: Firestore = FirestoreClient.getFirestore()
+    val firebaseFirestore = FirestoreClient.getFirestore()
 
-    configureRouting(oneSignalService, oneSignalAppId, firebaseFirestore)
+    configureRouting(
+        oneSignalService,
+        oneSignalAppId,
+        firebaseFirestore,
+    )
 }
