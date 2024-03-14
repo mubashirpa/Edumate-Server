46b14b37720dc40b1ff160e7a34dab128aa5d6f4
Mubashir P A <mubashirpa2002@gmail.com>
2024-03-14 20:33:44 +0530

    Added Experimental annotation.

---
+++ src/main/kotlin/app/edumate/server/core/Annotation.kt
@@ -0,0 +1,5 @@
+package app.edumate.server.core
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.EXPRESSION)
+annotation class Experimental
