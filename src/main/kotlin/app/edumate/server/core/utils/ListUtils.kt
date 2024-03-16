6539503fcd603a7105679cdcec78b4efd61710c8
Mubashir P A <mubashirpa2002@gmail.com>
2024-03-16 18:53:25 +0530

    Added ListUtils.

---
+++ src/main/kotlin/app/edumate/server/core/utils/ListUtils.kt
@@ -0,0 +1,15 @@
+package app.edumate.server.core.utils
+
+object ListUtils {
+    fun <T> moveToIndex(
+        list: MutableList<T>,
+        index: Int,
+        predicate: (T) -> Boolean,
+    ) {
+        val itemIndex = list.indexOfFirst(predicate)
+        if (itemIndex != -1 && itemIndex != index) {
+            val item = list.removeAt(itemIndex)
+            list.add(index, item)
+        }
+    }
+}
