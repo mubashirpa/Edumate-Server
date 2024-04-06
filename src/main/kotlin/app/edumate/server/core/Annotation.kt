63ee8090960fc9586de6b285e5bb1110725367a4
Mubashir P A <mubashirpa2002@gmail.com>
2024-04-06 14:22:14 +0530

    Deleted Annotation.kt

--- src/main/kotlin/app/edumate/server/core/Annotation.kt
+++
@@ -1,5 +0,0 @@
-package app.edumate.server.core
-
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.EXPRESSION)
-annotation class Experimental
