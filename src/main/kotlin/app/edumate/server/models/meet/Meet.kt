13e042e9413f0c2f7f1cf9d4adf0832cc9bca0c5
Mubashir P A <mubashirpa2002@gmail.com>
2024-04-09 15:15:38 +0530

    Optimised code

--- src/main/kotlin/app/edumate/server/models/meet/Meet.kt
+++ src/main/kotlin/app/edumate/server/models/meet/Meet.kt
@@ -5,10 +5,13 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Meet(
     val alternateLink: String? = null,
+    val callType: String? = null,
     val courseId: String? = null,
     val creationTime: String? = null,
     val creatorUserId: String? = null,
+    val endTime: String? = null,
     val id: String? = null,
+    val startTime: String? = null,
     val state: MeetState? = null,
     val updateTime: String? = null,
 )
--- src/main/kotlin/app/edumate/server/routes/MeetRoutes.kt
+++ src/main/kotlin/app/edumate/server/routes/MeetRoutes.kt
@@ -71,12 +71,14 @@ fun Route.meetRouting(
             if (havePermission) {
                 val id = DatabaseUtils.generateId(12)
                 val alternateLink = "https://getstream.io/video/$courseId/join/$id"
+                val callType = "default"
                 val creationTime = DateTimeUtils.getCurrentDateTime("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
                 val state = MeetState.CREATED
 
                 val meet =
                     Meet(
                         alternateLink = alternateLink,
+                        callType = callType,
                         courseId = courseId,
                         creationTime = creationTime,
                         creatorUserId = userId,
@@ -150,12 +152,74 @@ fun Route.meetRouting(
 
             if (havePermission) {
                 val reference = meetDatabase(database, courseId, id)
+
                 reference.removeValueAsync().get()
 
                 call.respondText(
                     text = "The meet with id $id was deleted",
                     status = HttpStatusCode.Accepted,
                 )
+            } else {
+                call.respondText(
+                    text = "You don't have permission",
+                    status = HttpStatusCode.Forbidden,
+                )
+            }
+        }
+        get("/{id}") {
+            val token =
+                call.request.header(HttpHeaders.Authorization) ?: return@get call.respondText(
+                    text = "No token provided",
+                    status = HttpStatusCode.Unauthorized,
+                )
+            if (token.isBlank()) {
+                return@get call.respondText(
+                    text = "Only valid authentication supported",
+                    status = HttpStatusCode.BadRequest,
+                )
+            }
+            val userId =
+                try {
+                    FirebaseUtils.getUserIdFromToken(token)
+                } catch (e: FirebaseAuthException) {
+                    when (e.authErrorCode.name) {
+                        "EXPIRED_ID_TOKEN" -> return@get call.respondText(
+                            text = "The access token is expired",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+
+                        "REVOKED_ID_TOKEN" -> return@get call.respondText(
+                            text = "The access token has been revoked",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+
+                        else -> return@get call.respondText(
+                            text = "The provided access token is not valid",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+                    }
+                }
+            val courseId =
+                call.parameters["courseId"] ?: return@get call.respondText(
+                    text = "You must specify a course id",
+                    status = HttpStatusCode.BadRequest,
+                )
+            val id =
+                call.parameters["id"] ?: return@get call.respondText(
+                    text = "You must specify a meet id",
+                    status = HttpStatusCode.BadRequest,
+                )
+
+            val course =
+                coursesStorage.find { it.id == courseId } ?: return@get call.respondText(
+                    text = "No course with id $courseId",
+                    status = HttpStatusCode.NotFound,
+                )
+            val havePermission =
+                course.teachers?.any { it.userId == userId } == true || course.students?.any { it.userId == userId } == true
+
+            if (havePermission) {
+                getMeet(call, database, courseId, id)
             } else {
                 call.respondText(
                     text = "You don't have permission",
@@ -211,9 +275,145 @@ fun Route.meetRouting(
                 course.teachers?.any { it.userId == userId } == true || course.students?.any { it.userId == userId } == true
 
             if (havePermission) {
-                val reference = database.getReference(courseId).child("meet")
+                listMeet(call, database, courseId)
+            } else {
+                call.respondText(
+                    text = "You don't have permission",
+                    status = HttpStatusCode.Forbidden,
+                )
+            }
+        }
+        post("/{id}:join") {
+            val token =
+                call.request.header(HttpHeaders.Authorization) ?: return@post call.respondText(
+                    text = "No token provided",
+                    status = HttpStatusCode.Unauthorized,
+                )
+            if (token.isBlank()) {
+                return@post call.respondText(
+                    text = "Only valid authentication supported",
+                    status = HttpStatusCode.BadRequest,
+                )
+            }
+            val userId =
+                try {
+                    FirebaseUtils.getUserIdFromToken(token)
+                } catch (e: FirebaseAuthException) {
+                    when (e.authErrorCode.name) {
+                        "EXPIRED_ID_TOKEN" -> return@post call.respondText(
+                            text = "The access token is expired",
+                            status = HttpStatusCode.Unauthorized,
+                        )
 
-                getMeet(call, reference)
+                        "REVOKED_ID_TOKEN" -> return@post call.respondText(
+                            text = "The access token has been revoked",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+
+                        else -> return@post call.respondText(
+                            text = "The provided access token is not valid",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+                    }
+                }
+            val courseId =
+                call.parameters["courseId"] ?: return@post call.respondText(
+                    text = "You must specify a course id",
+                    status = HttpStatusCode.BadRequest,
+                )
+            val id =
+                call.parameters["id"] ?: return@post call.respondText(
+                    text = "You must specify a meet id",
+                    status = HttpStatusCode.BadRequest,
+                )
+
+            val course =
+                coursesStorage.find { it.id == courseId } ?: return@post call.respondText(
+                    text = "No course with id $courseId",
+                    status = HttpStatusCode.NotFound,
+                )
+            val havePermission = course.teachers?.any { it.userId == userId } == true
+
+            if (havePermission) {
+                val reference = meetDatabase(database, courseId, id)
+                val updates: MutableMap<String, Any> = HashMap()
+                val startTime = DateTimeUtils.getCurrentDateTime("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                updates["startTime"] = startTime
+                updates["state"] = MeetState.IN_PROGRESS
+                updates["updateTime"] = startTime
+
+                reference.updateChildrenAsync(updates).get()
+
+                call.respond(HttpStatusCode.OK)
+            } else {
+                call.respondText(
+                    text = "You don't have permission",
+                    status = HttpStatusCode.Forbidden,
+                )
+            }
+        }
+        post("/{id}:leave") {
+            val token =
+                call.request.header(HttpHeaders.Authorization) ?: return@post call.respondText(
+                    text = "No token provided",
+                    status = HttpStatusCode.Unauthorized,
+                )
+            if (token.isBlank()) {
+                return@post call.respondText(
+                    text = "Only valid authentication supported",
+                    status = HttpStatusCode.BadRequest,
+                )
+            }
+            val userId =
+                try {
+                    FirebaseUtils.getUserIdFromToken(token)
+                } catch (e: FirebaseAuthException) {
+                    when (e.authErrorCode.name) {
+                        "EXPIRED_ID_TOKEN" -> return@post call.respondText(
+                            text = "The access token is expired",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+
+                        "REVOKED_ID_TOKEN" -> return@post call.respondText(
+                            text = "The access token has been revoked",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+
+                        else -> return@post call.respondText(
+                            text = "The provided access token is not valid",
+                            status = HttpStatusCode.Unauthorized,
+                        )
+                    }
+                }
+            val courseId =
+                call.parameters["courseId"] ?: return@post call.respondText(
+                    text = "You must specify a course id",
+                    status = HttpStatusCode.BadRequest,
+                )
+            val id =
+                call.parameters["id"] ?: return@post call.respondText(
+                    text = "You must specify a meet id",
+                    status = HttpStatusCode.BadRequest,
+                )
+
+            val course =
+                coursesStorage.find { it.id == courseId } ?: return@post call.respondText(
+                    text = "No course with id $courseId",
+                    status = HttpStatusCode.NotFound,
+                )
+            val havePermission = course.teachers?.any { it.userId == userId } == true
+
+            if (havePermission) {
+                val reference = meetDatabase(database, courseId, id)
+                val updates: MutableMap<String, Any> = HashMap()
+                val endTime = DateTimeUtils.getCurrentDateTime("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                updates["endTime"] = endTime
+                updates["state"] = MeetState.DISCONNECTED
+                updates["updateTime"] = endTime
+
+                reference.updateChildrenAsync(updates).get()
+
+                call.respond(HttpStatusCode.OK)
             } else {
                 call.respondText(
                     text = "You don't have permission",
@@ -235,11 +435,56 @@ private fun meetDatabase(
 @OptIn(ExperimentalCoroutinesApi::class)
 private suspend fun getMeet(
     call: ApplicationCall,
-    reference: DatabaseReference,
+    database: FirebaseDatabase,
+    courseId: String,
+    id: String,
 ) {
+    val reference = meetDatabase(database, courseId, id)
     val snapshot =
         withContext(Dispatchers.IO) {
-            suspendCancellableCoroutine<DataSnapshot?> { continuation ->
+            suspendCancellableCoroutine { continuation ->
+                val listener =
+                    object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot?) {
+                            continuation.resume(dataSnapshot) {
+                                // Clean-up code if needed
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError?) {
+                            continuation.resume(null) {
+                                // Clean-up code if needed
+                            }
+                        }
+                    }
+                reference.addListenerForSingleValueEvent(listener)
+                continuation.invokeOnCancellation {
+                    reference.removeEventListener(listener)
+                }
+            }
+        }
+    val meet = snapshot?.getValue(Meet::class.java)
+
+    if (meet != null) {
+        call.respond(meet)
+    } else {
+        call.respondText(
+            text = "No meet with id $id",
+            status = HttpStatusCode.NotFound,
+        )
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+private suspend fun listMeet(
+    call: ApplicationCall,
+    database: FirebaseDatabase,
+    courseId: String,
+) {
+    val reference = database.getReference(courseId).child("meet")
+    val snapshot =
+        withContext(Dispatchers.IO) {
+            suspendCancellableCoroutine { continuation ->
                 val listener =
                     object : ValueEventListener {
                         override fun onDataChange(dataSnapshot: DataSnapshot?) {
